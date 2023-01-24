68608664ad2869238f6b3fd400af5a466d62a16e
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 13:48:28 +0100

    function

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,49 @@ console.log(anfangsIndexSusi);          // 0  fängt bei 0 an
 let ausschneidernSusi = A.slice(anfangsIndexSusi, längeSusi);
 console.log(ausschneidernSusi);          // Susi 
 
-document.write(ausschneidernSusi);    // in Html einfügen
+document.write(ausschneidernSusi + " ");    // in Html einfügen
 
 
+
+// Funktion
+// input Variable A = x 
+// und Suche y = namen 
+function inHtml(x, y){      // x = A    y = Susi 
+    let A = x;
+    console.log("function " + A);
+    let sucheSusi = y;
+    console.log("function " + sucheSusi);
+
+
+    let längeSusi = sucheSusi.length;
+console.log(längeSusi);                 // 4  4 Zeichen lang
+let anfangsIndexSusi = A.indexOf(sucheSusi);
+console.log(anfangsIndexSusi);          // 0  fängt bei 0 an
+
+let ausschneidernSusi = A.slice(anfangsIndexSusi, längeSusi);
+console.log(ausschneidernSusi);          // Susi 
+
+document.write(ausschneidernSusi + " ");    // in Html einfügen
+
+}
+
+inHtml(A, sucheSusi);
+inHtml(A, sucheIsGoingToSchool);
+inHtml(A, sucheIs);
+inHtml(A, sucheSchool);
+inHtml(A, sucheSusiIsSchool);
+
+
+let sucheIs2 = "is";
+
+let längeIs2 = sucheIs2.length;
+console.log(längeIs2);                 // 2  2 Zeichen lang
+let anfangsIndexIs2 = A.indexOf(sucheIs2);
+console.log(anfangsIndexIs2);          // 5  fängt bei 5 an
+
+let ausschneidernIs2 = A.slice(anfangsIndexIs2, längeIs2);
+console.log(ausschneidernIs2);          // Susi 
+
+document.write(ausschneidernIs2 + " ");    // in Html einfügen
+
+console.log(A);
